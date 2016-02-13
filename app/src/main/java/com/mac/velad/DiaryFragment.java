8bbb6f4251b58bd79c3a9a547484ab6e0efb34b9
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-02-13 18:49:58 +0100

    Implement section fragments and options menu

---
+++ app/src/main/java/com/mac/velad/DiaryFragment.java
@@ -0,0 +1,58 @@
+package com.mac.velad;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DiaryFragment extends Fragment {
+
+    public DiaryFragment() {
+        // Required empty public constructor
+    }
+
+    public static DiaryFragment newInstance() {
+        DiaryFragment fragment = new DiaryFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setupFloatingButton();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_diary, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.fragment_diary_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void setupFloatingButton() {
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.ic_email_white_24dp);
+        if (!fab.isShown()) {
+            fab.show();
+        }
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/mac/velad/MainActivity.java
+++ app/src/main/java/com/mac/velad/MainActivity.java
@@ -3,6 +3,7 @@ package com.mac.velad;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,17 +22,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(getTitleForMenuId(R.id.nav_today));
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +33,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setCheckedItem(R.id.nav_today);
         navigationView.setNavigationItemSelectedListener(this);
+
+        navigate(R.id.nav_today);
     }
 
     @Override
@@ -57,20 +50,37 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        getSupportActionBar().setTitle(getTitleForMenuId(id));
+        navigate(id);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    public String getTitleForMenuId(int id) {
-        switch (id) {
+    private void navigate(int menuId) {
+        getSupportActionBar().setTitle(getTitle(menuId));
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, getFragment(menuId)).commit();
+    }
+
+    private String getTitle(int menuId) {
+        switch (menuId) {
             case R.id.nav_today: return getString(R.string.menu_item_title_today);
             case R.id.nav_diary: return getString(R.string.menu_item_title_diary);
             case R.id.nav_week: return getString(R.string.menu_item_title_week);
             case R.id.nav_reports: return getString(R.string.menu_item_title_reports);
             case R.id.nav_settings: return getString(R.string.menu_item_title_settings);
+            default: return null;
+        }
+    }
+
+    private Fragment getFragment(int menuId) {
+        switch (menuId) {
+            case R.id.nav_today: return TodayFragment.newInstance();
+            case R.id.nav_diary: return DiaryFragment.newInstance();
+            case R.id.nav_week: return WeekFragment.newInstance();
+            case R.id.nav_reports: return ReportsFragment.newInstance();
+            case R.id.nav_settings: return SettingsFragment.newInstance();
             default: return null;
         }
     }
